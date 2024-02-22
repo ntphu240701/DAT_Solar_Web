25ab04c3a91a6c0a4a186945307ee1fa7b6bd91f
Tai Ngo <ndtt.2712@gmail.com>
2024-02-22 09:10:44 +0700

    TAI

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useIntl } from 'react-intl';
 import axios from 'axios';
 import { host } from './component/Lang/Contant';
 import adminslice from "./component/Redux/adminslice";
+import { callApi } from './component/Api/Api';
 
 const Home = React.lazy(() => import('./component/Home/Home'));
 const Project = React.lazy(() => import('./component/Project/Project'));
@@ -38,30 +39,32 @@ function App() {
 
   useEffect(() => {
     const checkAuth = async () => {
-      await axios.get(host.AUTH + window.location.pathname, {
 
-        headers: {
-          token: JSON.parse(localStorage.getItem('token')) || JSON.parse(sessionStorage.getItem('token'))
-        },
-        withCredentials: true
+      let res = await callApi('get', host.AUTH + window.location.pathname)
+      if (res.status) {
+        console.log(res)
+        rootDispatch(adminslice.actions.setstatus(res.status))
+        rootDispatch(adminslice.actions.setusr(res.user))
+      } else {
+        setLoading(false)
       }
-      ).then(
-        function (res) {
-          if (res.data.status) {
-            console.log(res.data)
-            rootDispatch(adminslice.actions.setstatus(res.data.status))
-            rootDispatch(adminslice.actions.setusr(res.data.user))
-          } else {
-            setLoading(false)
-          }
-        }
-      )
     }
-    //SUA DE DANG NHAP OFFLINE
+
+    const checkApi = async () => {
+      let data = await callApi('post', host.AUTH + '/getInf', { usr: 'solar_master' })
+      //let data = await callApi('post', host.DATA + '/getPlant', { usr: 'solar_master' })
+      //let data = await callApi('post', host.DATA + '/getLogger', { plantid: '1' })
+      //let data = await callApi('post', host.DATA + '/addPlant', { usr: 'solar_master', name: 'solar 03', company:'DAT', addr: '123, đường 1, phường 2, quận 3', long: '10.123', lat: '106.123', contact: 'hoang', phone: '0928382825', business: 'DAT Group', type: 'industry', mode: 'solar', griddate: '02/20/2024', capacity:'123', angle: '123', currency: '123', price: '123', production: '123', power: '123'})
+      //let data = await callApi('post', host.DATA + '/dropPlant', {plantid: '4', usr: 'solar_master' })
+      //let data = await callApi('post', host.DATA + '/editPlant', {plantid: '3', usr: 'solar_master', name: 'solar 03', company:'Công ty Cổ Phần Tập Đoàn DAT', addr: '12 Đường Đông Hưng Thuận 10, Phường Đông Hưng Thuận, Quận 12, TP.HCM	', long: '10.8357066', lat: '106.6271617', contact: 'hoang', phone: '0928382825', business: 'DAT Group', type: 'industry', mode: 'solar', griddate: '02/20/2024', capacity:'500', angle: '0', currency: 'vnd', price: '1000', production: '500', power: '50'})
+      console.log("hell",data)
+
+    }
+
     rootDispatch(adminslice.actions.setstatus(true))
 
-    //SUA DE DANG NHAP ONLINE
     // checkAuth();
+    // checkApi();
   }, [])
 
   return (
