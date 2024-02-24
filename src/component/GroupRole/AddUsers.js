ddd09f48ef71fef1850634759cd3e9e068b74dce
Tai Ngo <ndtt.2712@gmail.com>
2024-02-24 15:36:48 +0700

    TAI

--- src/component/GroupRole/AddUsers.js
+++ src/component/GroupRole/AddUsers.js
@@ -7,59 +7,71 @@ import { signal } from "@preact/signals-react";
 
 const infouser = signal([
   {
-    name: "Tiến Bịp DAT",
-    username: "tiendat_012",
-    subinfo: "RnD Center",
+    username: "tantaingo",
+    name: "Tài Giỏi",
+    email: "tantai.ngo@datgroup.com.vn",
   },
   {
-    name: "Hiệp sĩ đường phố",
-    username: "hiepdat_012",
-    subinfo: "RnD Center",
-  },
-  {
-    name: "Johnny Trí Nguyễn",
-    username: "tridat_012",
-    subinfo: "RnD Center",
-  },
-  {
+    username: "loctp",
     name: "Tony Trần",
+    email: "locdat_012@datgroup.com.vn",
+  },
+  {
+    username: "tiendv",
+    name: "Tiến Bịp DAT",
+    email: "tiendat_012@datgroup.com.vn",
+  },
+  {
+    username: "hiepga",
+    name: "Hiệp sĩ đường phố",
+    email: "hiepdat_012@datgroup.com.vn",
+  },
+  {
+    username: "tridat",
+    name: "Johnny Trí Nguyễn",
+    email: "tridat_012@datgroup.com.vn",
+  },
+  {
     username: "tonydat_012",
-    subinfo: "RnD Center",
+    name: "Tony Trần",
+    email: "tonydat_012@datgroup.com.vn",
   },
   {
-    name: "Phó Lũ",
     username: "phudat_012",
-    subinfo: "RnD Center",
+    name: "Phú Hộ",
+    email: "phudat_012@datgroup.com.vn",
   },
   {
-    name: "Anh A",
     username: "anhadat_012",
-    subinfo: "RnD Center",
+    name: "Anh A",
+    email: "anhadat_012@datgroup.com.vn",
   },
   {
-    name: "Anh B",
     username: "anhbdat_012",
-    subinfo: "RnD Center",
+    name: "Anh B",
+    email: "anhadat_012@datgroup.com.vn",
   },
   {
     name: "Anh C",
     username: "anhcdat_012",
-    subinfo: "RnD Center",
+    email: "anhcdat_012@datgroup.com.vn",
   },
   {
     name: "Anh D",
     username: "anhddat_012",
-    subinfo: "RnD Center",
+    email: "anhddat_012@datgroup.com.vn",
   },
   {
     name: "Anh E",
     username: "anhedat_012",
-    subinfo: "RnD Center",
+    email: "anhddat_012@datgroup.com.vn",
   },
 ]);
 
 export default function AddUsers() {
   const [username, setUsername] = useState("");
+  const [addUserState, setAddUserState] = useState("none");
+
   const popup_state = {
     pre: { transform: "rotate(0deg)", transition: "0.5s", color: "black" },
     new: { transform: "rotate(90deg)", transition: "0.5s", color: "red" },
@@ -80,32 +92,48 @@ export default function AddUsers() {
   };
 
   const handleAddUser = () => {
-    addState.value = false;
+    // addState.value = false;
     const i = groupUser.value.findIndex(
       (item) => item.groupid === groupID.value
     );
-    // console.log(i);
     const check = groupUser.value[i].users.findIndex(
       (item) => item.username === username
     );
     if (check === -1) {
       const t = infouser.value.find((item) => item.username === username);
-      if(t !== undefined){
-        // groupUser.value[i].users.push(t);
+      if (t !== undefined) {
         groupUser.value[i] = {
           ...groupUser.value[i],
-          users:[
-            ...groupUser.value[i].users,
-            t
-          ]
-        }
-        console.log(groupUser.value);
+          users: [...groupUser.value[i].users, t],
+        };
+        setAddUserState("AddSuccess");
       } else {
-        alert("Không tim thay người dùng, vui lòng kiểm tra tên tài khoản.");
+        setAddUserState("UserNotFound");
       }
     } else {
-      alert("Người dùng đã ở trong nhóm.");
+      setAddUserState("UserAlreadyInGroup");
     }
+  };
+  const AddButton = () => {
+    return (
+      <div className="DAT_AddUserPopup_Box_Foot">
+        <button
+          style={{
+            border: "1px solid #505050",
+            backgroundColor: "white",
+            color: "#505050",
+          }}
+        >
+          Hủy
+        </button>
+        <button
+          style={{ backgroundColor: "#048FFF", color: "white" }}
+          onClick={() => handleAddUser()}
+        >
+          Xác nhận
+        </button>
+      </div>
+    );
   };
 
   return (
@@ -128,31 +156,86 @@ export default function AddUsers() {
       </div>
       <div className="DAT_AddUserPopup_Box_Body">
         <div className="DAT_AddUserPopup_Box_Body_Input">
-          <span>Tên tài khoản:</span>
-          <input
-            type="text"
-            required
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          {(() => {
+            switch (addUserState) {
+              case "AddSuccess":
+                return <p>Người dùng được thêm thành công !</p>;
+              case "UserNotFound":
+                return <p>Không tìm thấy người dùng, vui lòng kiểm tra lại.</p>;
+              case "UserAlreadyInGroup":
+                return <p>Người dùng này đã tồn tại trong nhóm.</p>;
+              default:
+                return (
+                  <>
+                    <span>Tên tài khoản:</span>
+                    <input
+                      type="text"
+                      required
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </>
+                );
+            }
+          })()}
         </div>
       </div>
-      <div className="DAT_AddUserPopup_Box_Foot">
-        <button
-          style={{
-            border: "1px solid #505050",
-            backgroundColor: "white",
-            color: "#505050",
-          }}
-        >
-          Hủy
-        </button>
-        <button
-          style={{ backgroundColor: "#048FFF", color: "white" }}
-          onClick={() => handleAddUser()}
-        >
-          Xác nhận
-        </button>
-      </div>
+      {(() => {
+        switch (addUserState) {
+          case "AddSuccess":
+            return (
+              <div className="DAT_AddUserPopup_Box_Foot">
+                <button
+                  onClick={() => (
+                    (addState.value = false), setAddUserState("none")
+                  )}
+                  style={{
+                    border: "1px solid #505050",
+                    backgroundColor: "white",
+                    color: "#505050",
+                  }}
+                >
+                  Thoát
+                </button>
+              </div>
+            );
+          case "UserNotFound":
+            return (
+              <div className="DAT_AddUserPopup_Box_Foot">
+                <button
+                  onClick={() => (
+                    (addState.value = false), setAddUserState("none")
+                  )}
+                  style={{
+                    border: "1px solid #505050",
+                    backgroundColor: "white",
+                    color: "#505050",
+                  }}
+                >
+                  Thoát
+                </button>
+              </div>
+            );
+          case "UserAlreadyInGroup":
+            return (
+              <div className="DAT_AddUserPopup_Box_Foot">
+                <button
+                  onClick={() => (
+                    (addState.value = false), setAddUserState("none")
+                  )}
+                  style={{
+                    border: "1px solid #505050",
+                    backgroundColor: "white",
+                    color: "#505050",
+                  }}
+                >
+                  Thoát
+                </button>
+              </div>
+            );
+          default:
+            return <AddButton />;
+        }
+      })()}
     </div>
   );
 }
