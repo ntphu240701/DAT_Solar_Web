b6826dac53ae91b22075af9396c77a34dbc4f1a0
ntphu <ntphu240701@gmail.com>
2024-03-22 10:19:20 +0700

    Phu_Update: api project, watchlist

--- src/component/Project/Project.js
+++ src/component/Project/Project.js
@@ -573,7 +573,7 @@ export default function Project(props) {
 
     return () => {
       plantState.value = "default";
-      projtab.value = "total";
+      // projtab.value = "total";
     };
   }, []);
 
